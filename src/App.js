3d0bcabc2cefd7938008cc5accfbd8b22ea5049a
Rohith Kumar <sairohitkulkarni839@gmail.com>
2025-06-08 20:29:23 +0530

    feat: home and app change

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Home from './components/Home'
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Home/>
+    <div className="App">
+      <h1>Welcome to my project</h1>
+    </div>
+    </>
+  )
+}
+
+export default App;
